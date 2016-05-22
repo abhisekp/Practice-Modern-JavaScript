59c49b2eaaaa58f8287d3ba149c0b545cf1bc521
Abhisek Pattnaik <abhisekp@engineer.com>
2016-05-22 08:19:12 +0530

    Refactor example codes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import 'babel-polyfill';
-import './fib-gen';
+// @flow
 
+import 'source-map-support/register';
+import 'babel-polyfill';
+import 'colors';
+// import async from 'async';
+// import _ from 'lodash';
+// import console from 'better-console';
+
+// import './fib-gen';
+import './table';
+import './random-throw';
