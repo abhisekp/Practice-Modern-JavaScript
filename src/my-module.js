149faed040c383c038e044fddf098f1a8ce70f08
Abhisek Pattnaik <abhisekp@engineer.com>
2016-05-23 02:58:16 +0530

    Use utility and fun packages

--- src/my-module.js
+++ src/my-module.js
@@ -1,1 +1,40 @@
-export const name = 'My Module';
+import memGen from './example/memoized-fib-gen';
+import console from 'better-console';
+import ordinal from 'ordinal-number-suffix';
+import numd from 'numd';
+import dedent from 'dedent';
+import ora from 'ora';
+import FONTS from 'cfonts';
+
+const fonts = new FONTS({
+  text: 'Fibonacci Gen',
+  font: 'block',
+  colors: '',
+  background: 'black',
+  letterSpacing: 1,
+  space: true,
+  maxLength: 10,
+});
+
+const spinner = ora({
+  text: 'Generating Fibonacci',
+  color: 'green',
+});
+
+spinner.start();
+setTimeout(() => {
+  const time = process.hrtime();
+  const maxNum = 65536;
+  const allFibs = [...memGen(maxNum)];
+
+  const diff = process.hrtime(time);
+  const nsTime = diff[0] * 1e9 + diff[1];
+  spinner.stop();
+  spinner.clear();
+
+  console.info(String(allFibs[maxNum]).green.inverse);
+  console.info(dedent`
+    ${ ordinal(maxNum).blue.inverse } fibonacci number \
+    generated in ${ numd(nsTime, 'nanosecond', 'nanoseconds') }
+  `);
+}, 0);
