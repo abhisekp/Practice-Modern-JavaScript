ed160b9c40e47070fc959e2655352f558ddbda88
Abhisek Pattnaik <abhisekp@engineer.com>
2016-05-22 08:29:27 +0530

    Add fib-gen

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import 'babel-polyfill';
 import 'colors';
 // import async from 'async';
 // import _ from 'lodash';
-// import console from 'better-console';
+import console from 'better-console';
 
-// import './fib-gen';
+import './fib-gen';
 import './table';
 import './random-throw';
