d1ad583a8f9d6ea38ddedc6999dda927a4522e21
Abhisek Pattnaik <abhisekp@engineer.com>
2016-05-23 02:05:01 +0530

    Import my-module

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@
 import 'source-map-support/register';
 import 'babel-polyfill';
 import 'colors';
+
 // import async from 'async';
 // import _ from 'lodash';
 // import console from 'better-console';
 
+import './my-module';
