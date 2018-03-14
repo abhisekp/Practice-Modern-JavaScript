8f8f78c908db4c6c685b3c18734dda6c34eee878
Abhisek Pattnaik <abhisekp@engineer.com>
2018-03-14 12:33:17 +0530

    Add lodash flow example and flowtype

--- src/features/function-piping.js
+++ src/features/function-piping.js
@@ -1,42 +1,26 @@
-import { curryN } from 'lodash/fp';
+import { flow, snakeCase, get, toUpper } from 'lodash/fp';
 
 function fnPipe() {
-  const get = curryN(2, (prop: string, obj: object): any => {
-    if (obj !== null && typeof obj === 'object') {
-      return obj[prop];
-    }
-
-    return undefined;
-  });
-
-  const uppercase = (string: string): string => {
-    if (typeof string === 'string') {
-      return string.toUpperCase();
-    }
-
-    return '';
-  };
-
-  const snaked = (string: string): string => {
-    if (typeof string === 'string') {
-      return `_${string.trim()}_`;
-    }
-
-    return '_';
-  };
-
   const obj = {
     name: 'Abhisek Pattnaik',
   };
 
-  const nonPipedResult = snaked(uppercase(get('name')(obj)));
+  const nonPipedResult = toUpper(snakeCase(get('name')(obj)));
   console.log('Non Piped:', nonPipedResult);
+
+  const flowedResult = flow(
+    get('name'),
+    snakeCase,
+    toUpper,
+  )(obj);
+
+  console.log('Flowed:', flowedResult);
 
   // 😁
   const pipedResult = obj
     |> get('name')
-    |> uppercase
-    |> snaked;
+    |> snakeCase
+    |> toUpper;
 
   console.log('Piped:', pipedResult);
 }
